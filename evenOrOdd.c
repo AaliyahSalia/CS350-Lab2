338efb5425596b2cc0a9bfdaeb3c2cf3808311f2
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2023-01-17 15:00:13 -0800

    Update evenOrOdd.c

--- evenOrOdd.c
+++ evenOrOdd.c
@@ -2,13 +2,13 @@
 
 int main(void){
     int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    if (num % 2 == 0){
-        printf("%d is even", num);
+    printf("Enter a number: "); //prompts the user to enter a number
+    scanf("%d", &num); //reads the input from user and stores it in the variable 'num'.
+    if (num % 2 == 0){ //checks if the number is divisible by 2.
+        printf("%d is even", num); //displays message if it's divisible by 2.
     }
     else{
-        printf("%d is odd", num);
+        printf("%d is odd", num); //displays message if it's not divisible by 2, then it's odd.
     }
     return 0;
 }
