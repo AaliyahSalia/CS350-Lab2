6eb4327a2298309dd9574dcd582fb1826a09a88e
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2023-01-17 15:07:13 -0800

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -9,24 +9,24 @@
 int main(void){
 
     int year;
-    printf("Enter a year: ");
-    scanf("%d", &year);
+    printf("Enter a year: "); // prompts the user to enter a year
+    scanf("%d", &year);  // reads the input from user and stores in variable 'year'
 
-    if (year % 4 == 0){
-        if (year % 100 == 0){
-            if (year % 400 == 0){
-                printf("%d is a leap year.", year);
+    if (year % 4 == 0){ // checks if the year is divisible by 4
+        if (year % 100 == 0){ // if it is divisible by 4, it checks if it's divisible by 100
+            if (year % 400 == 0){ // if it's divisible by 100, it checks if it's divisible by 400
+                printf("%d is a leap year.", year); // if it's divisible by 400 it is a leap year
             }
             else{
-                printf("%d is not a leap year.", year);
+                printf("%d is not a leap year.", year); // if it's not divisible by 400 it is not a leap year
             }
         }
         else{
-            printf("%d is a leap year.", year);
+            printf("%d is a leap year.", year); // if it's not divisible by 100 it is a leap year
         }
     }
     else{
-        printf("%d is not a leap year.", year);
+        printf("%d is not a leap year.", year); // if it's not divisible by 4 it is not a leap year
     }
     return 0;
 }
