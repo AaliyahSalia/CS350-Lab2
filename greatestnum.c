22e4b74377788590304e15428d9fc360fa8c4caf
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2023-01-17 15:02:51 -0800

    Update greatestnum.c

--- greatestnum.c
+++ greatestnum.c
@@ -5,16 +5,16 @@
 
 int main(void){
     int a, b, c;
-    printf("Enter value for a, b & c: ");
-    scanf("%d %d %d", &a, &b, &c);
-    if (a > b && a > c){
-        printf("a is greatest");
+    printf("Enter value for a, b & c: "); //prompts the user to enter the values of a, b, and c
+    scanf("%d %d %d", &a, &b, &c); //reads the input from user and stores in variable 'a', 'b', 'c'
+    if (a > b && a > c){ //checks if a is greater than b and c
+        printf("a is greatest"); //displays message if a is greater.
     }
-    else if (b > a && b > c){
-        printf("b is greatest");
+    else if (b > a && b > c){ //checks if b is greater than a and c
+        printf("b is greatest"); //displays message if b is greater.
     }
     else{
-        printf("c is greatest");
+        printf("c is greatest"); //if none of above conditions are true then it means c is the greatest.
     }
     return 0;
 }
