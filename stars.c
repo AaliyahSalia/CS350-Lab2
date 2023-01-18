94a20035b6d6ba0bab2f51fe1fed54f3e58e0c90
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2023-01-17 16:26:52 -0800

    Update stars.c

--- stars.c
+++ stars.c
@@ -2,31 +2,31 @@
 
 int main() {
     int n;
-    printf("Enter the number of rows: ");
-    scanf("%d", &n);
+    printf("Enter the number of rows: "); //prompts the user to enter the number of rows
+    scanf("%d", &n); //reads the input from user and stores in variable 'n'.
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; j++) {
+    for (int i = 1; i <= n; i++) { // outer loop to iterate through rows
+        for (int j = 1; j <= n - i; j++) { // inner loop to print spaces in the beginning of each row
             printf(" ");
         }
 
-        for (int j = 1; j <= (2 * i) - 1; j++) {
+        for (int j = 1; j <= (2 * i) - 1; j++) { // inner loop to print '*' in each row
             printf("*");
         }
 
-        printf("\n");
+        printf("\n");  // move to next line
     }
 
-    for (int i = n - 1; i >= 1; i--) {
-        for (int j = 1; j <= n - i; j++) {
+    for (int i = n - 1; i >= 1; i--) {   // outer loop to iterate through rows in reverse
+        for (int j = 1; j <= n - i; j++) { // inner loop to print spaces in the beginning of each row
             printf(" ");
         }
 
-        for (int j = 1; j <= (2 * i) - 1; j++) {
+        for (int j = 1; j <= (2 * i) - 1; j++) { // inner loop to print '*' in each row
             printf("*");
         }
 
-        printf("\n");
+        printf("\n"); // move to next line
     }
     return 0;
 }
